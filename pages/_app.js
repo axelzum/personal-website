4e13065fce1ba82c9af225c30db9af53e2e99ffe
Axel Zumwalt <axelzum@iastate.edu>
2021-08-28 13:38:51 -0500

    Serperate theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
 import Head from 'next/head';
 
 import Layout from '../components/layout';
+import Theme from "../public/theme";
 
 const GlobalStyle = createGlobalStyle`
   html, body {
@@ -20,32 +21,27 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const theme = {
-  colors: {
-    primary: '#223286',
-  },
-};
-
-const AppLayout = ({ Component, pageProps }) => (
-  <Layout>
-    <Head>
-      <title>{'Axel Zumwalt Personal Website'}</title>
-      <link href="/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180" />
-      <link href="/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png" />
-      <link href="/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png" />
-      <link href="/favicons/site.webmanifest" rel="manifest" />
-      <link color="#223286" href="/favicons/safari-pinned-tab.svg" rel="mask-icon" />
-      <link href="/favicons/favicon.ico" rel="shortcut icon" />
-      <meta content="#223286" name="msapplication-TileColor" />
-      <meta content="/favicons/browserconfig.xml" name="msapplication-config" />
-      <meta content="#ffffff" name="theme-color" />
-      <link href="https://use.typekit.net/oov2wcw.css" rel="stylesheet" />
-    </Head>
-    <GlobalStyle />
-    <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeProvider>
-  </Layout>
-);
+const AppLayout = ({Component, pageProps}) => (
+    <Theme>
+      <GlobalStyle/>
+      <Layout>
+        <Head>
+          <title>{'Axel Zumwalt Personal Website'}</title>
+          <link href="/favicons/apple-touch-icon.png" rel="apple-touch-icon" sizes="180x180"/>
+          <link href="/favicons/favicon-32x32.png" rel="icon" sizes="32x32" type="image/png"/>
+          <link href="/favicons/favicon-16x16.png" rel="icon" sizes="16x16" type="image/png"/>
+          <link href="/favicons/site.webmanifest" rel="manifest"/>
+          <link color="#223286" href="/favicons/safari-pinned-tab.svg" rel="mask-icon"/>
+          <link href="/favicons/favicon.ico" rel="shortcut icon"/>
+          <meta content="#223286" name="msapplication-TileColor"/>
+          <meta content="/favicons/browserconfig.xml" name="msapplication-config"/>
+          <meta content="#ffffff" name="theme-color"/>
+          <link href="https://use.typekit.net/oov2wcw.css" rel="stylesheet"/>
+        </Head>
+        <Component {...pageProps} />
+      </Layout>
+    </Theme>
+  )
+;
 
 export default AppLayout;
---
+++ public/theme.js
@@ -0,0 +1,13 @@
+import {ThemeProvider} from "styled-components";
+
+const theme = {
+  colors: {
+    primary: '#223286',
+  },
+};
+
+const Theme = ({children}) => (
+  <ThemeProvider theme={theme}> {children}</ThemeProvider>
+);
+
+export default Theme;
