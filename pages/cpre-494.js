18c1de54150fc7f9d15dbbf0d37fc7812762756f
username <axelzum@gmail.com>
2021-10-12 01:35:04 -0500

    494 essays
    Signed-off-by: username <axelzum@gmail.com>

--- pages/cpre-494.js
+++ pages/cpre-494.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 
 import InfoSection from '../components/info-section/info-section';
 import EthicsEssay from '../components/cpre-494/ethics-essay';
+import GeneralEducationReflection from '../components/cpre-494/general-education-reflection';
+import CumulativeReflection from '../components/cpre-494/cumulative-reflection';
 
 import { StyledInfoPageContainer, StyledTitleBox } from './styles';
 
@@ -17,6 +19,8 @@ const Cpre494 = () => (
         <h1>{'CprE 494 Senior Portfolio Assignments'}</h1>
         <h3>{'This is a collection of assignments to meet the requirements for the CprE 494 Senior Portfolio'}</h3>
       </StyledTitleBox>
+      <InfoSection collapse={<GeneralEducationReflection />} header='General Education Reflection' info='' subheader='' />
+      <InfoSection collapse={<CumulativeReflection />} header='Cumulative Reflection' info='' subheader='' />
       <InfoSection collapse={<EthicsEssay />} header='CprE 394 Ethics Essay' info='' subheader='' />
     </StyledInfoPageContainer>
   </>
