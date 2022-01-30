639b02779e57305ce1b907242d6c8f80fc14cdab
Axel Zumwalt <axelzum@iastate.edu>
2022-01-30 15:32:35 -0600

    Small footer style changes

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,35 +1,6 @@
-import styled from 'styled-components';
 
 import IconLinks from './icon-links';
-
-const StyledFooter = styled.footer`
-  width: 100%;
-  height: 80px;
-  border-top: thin solid;
-  margin-top: 30px;
-  //display: flex;
-  //flex-wrap: wrap;
-  //justify-content: space-between;
-  //align-items: center;
-`;
-
-const StyledFooterInfoContainer = styled.div`
-  max-width: 1400px;
-  margin: auto;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const StyledContactInfo = styled.div`
-  padding-left: 30px;
-  position: relative;
-  left: 0;
-
-  p {
-    margin: 0;
-  }
-`;
+import {StyledContactInfo, StyledCopyright, StyledFooter, StyledFooterInfoContainer} from './styles';
 
 const Footer = () => (
   <StyledFooter>
@@ -38,7 +9,7 @@ const Footer = () => (
         <p>{'axelzum@gmail.com'}</p>
         <p>{'319-389-8064'}</p>
       </StyledContactInfo>
-      <p>{'Axel Zumwalt ©2022'}</p>
+      <StyledCopyright>{'Axel Zumwalt ©2022'}</StyledCopyright>
       <IconLinks/>
     </StyledFooterInfoContainer>
   </StyledFooter>
